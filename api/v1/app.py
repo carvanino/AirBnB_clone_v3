faa4e15c6c6b51884e1952038f2374573d2be1f7
Isquare007 <akibrahiim2019@gmail.com>
2023-01-22 23:06:20 +0000

    the app

---
+++ api/v1/app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""app v1"""
+
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_session(exception):
+    """closes storage"""
+    storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == "__main__":
+    if getenv('HBNB_API_HOST'):
+        HOST = getenv('HBNB_API_HOST')
+    else:
+        HOST = '0.0.0.0'
+
+    if getenv('HBNB_API_PORT'):
+        PORT = getenv('HBNB_API_PORT')
+    else:
+        PORT = '5000'
+    app.run(host=HOST, port=PORT, threaded=True)
