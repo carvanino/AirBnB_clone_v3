068852df586bd5f1cc31fc1c406e6b8e36071d9d
Isquare007 <akibrahiim2019@gmail.com>
2023-01-23 17:56:26 +0000

    new 8

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,70 +1,61 @@
-#!/usr/bin/python3
-"""
-State api module
-"""
-import models
+#!/usr/bin/python
+"""iew for State objects that handles all default RESTFul API actions:"""
+
+from api.v1.views import app_views
+from flask import jsonify, abort, make_response, request
 from models import storage
 from models.state import State
-from flask import jsonify, abort, request, make_response
-from api.v1.views import app_views
 
-
-@app_views.route('/states', methods=['GET'])
-def get_states():
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def states():
     """ Retrieves the list of all State objects """
-    states = models.storage.all(State)
-    stateList = []
-    for state in states.values():
-        stateList.append(state.to_dict())
-    return jsonify(stateList)
+    all_states = storage.all(State)
+    return jsonify([obj.to_dict() for obj in all_states.values()])
 
 
-@app_views.route('/states/<state_id>', methods=['GET'])
-def get_state(state_id):
-    """ Retrieve a state object by id """
-    states = models.storage.all(State)
-    for state in states.values():
-        if state_id in state.to_dict().values():
-            return jsonify(state.to_dict())
-    abort(404)
-
-
-@app_views.route('/states/', methods=['POST'])
-def create_state():
-    """ Create a new instance of state """
-    if not request.json:
-        abort(400, description='Not a JSON')
-    if 'name' not in request.json:
-        abort(400, description='Missing name')
-    state = request.get_json()
-    new_state = State(**state)  # initialized as kwargs
-    new_state.save()
-    # return make_response(jsonify(new_state.to_dict()), 201)
-    # both works
-    return (jsonify(new_state.to_dict()), 201)
-
-
-@app_views.route('/states/<state_id>', methods=['PUT'])
-def update_state(state_id):
-    """ Updates the attribute of a state object """
-    state = models.storage.get(State, state_id)
-    if state is None:
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def state(state_id):
+    """return a single state passed"""
+    obj = storage.get("State", state_id)
+    if not obj:
         abort(404)
-    if not request.json:
-        abort(400, description='Not a JSON')
-    stateAttr = request.get_json()
-    for key, value in stateAttr.items():
-        if key not in {'id', 'created_at', 'updated_at'}:
-            setattr(state, key, value)
-    state.save()
-    return jsonify(state.to_dict()), 200
+    return jsonify(obj.to_dict())
 
-
-@app_views.route('/states/<state_id>', methods=['DELETE'])
-def delete_state(state_id):
-    state = storage.get(State, state_id)
-    if state is None:
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def del_state_id(state_id):
+    """ Deletes a State object """
+    state = storage.get("State", state_id)
+    if not state:
         abort(404)
-    storage.delete(state)
+    state.delete()
     storage.save()
-    return jsonify({})
+    return make_response(jsonify({}), 200)
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def post_states():
+    """ Creates a State object """
+    new_state = request.get_json()
+    if not new_state:
+        abort(400, "Not a JSON")
+    if "name" not in new_state:
+        abort(400, "Missing name")
+    state = State(**new_state)
+    storage.new(state)
+    storage.save()
+    return make_response(jsonify(state.to_dict()), 201)
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def put_state_id(state_id):
+    """ Updates a State object """
+    state = storage.get("State", state_id)
+    if not state:
+        abort(404)
+    my_dict = request.get_json()
+    if not my_dict:
+        abort(400, "Not a JSON")
+    for key, value in my_dict.items():
+        if key != 'id' and key != 'created_at' and key != 'updated_at':
+            setattr(state, key, value)
+    storage.save()
+    return make_response(jsonify(state.to_dict()), 200)
