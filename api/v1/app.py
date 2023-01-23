6b47eb9163288de543d2d22a3a3aaacfb39023e6
Akinola <akinolatofunmi04@gmail.com>
2023-01-23 11:12:01 +0000

    Task 12 - CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,39 +1,36 @@
 #!/usr/bin/python3
-"""app v1"""
-
+"""
+Main Flask app module
+"""
 from flask import Flask, jsonify
+from os import getenv
 from models import storage
 from api.v1.views import app_views
-from os import getenv
-
+from flask import Blueprint
+from flask_cors import CORS
 
 app = Flask(__name__)
-# app_views.strict_slashes = False
-app.url_map.strict_slashes = False
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
+HBNB_API_HOST = getenv('HBNB_API_HOST') or '0.0.0.0'
+HBNB_API_PORT = getenv('HBNB_API_PORT') or 5000
+
+
 @app.teardown_appcontext
-def teardown_session(exception):
-    """closes storage"""
+def session_handler(self):
+    """ Close existing session of the sqlalchemy """
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
-    """ Page not found error handler """
-    return jsonify({"error": "Not found"}), 404
+def page_not_found(err):
+    """ Page Not Found handler """
+    return jsonify({'error': 'Not found'})
 
 
-if __name__ == "__main__":
-    if getenv('HBNB_API_HOST'):
-        HOST = getenv('HBNB_API_HOST')
-    else:
-        HOST = '0.0.0.0'
-
-    if getenv('HBNB_API_PORT'):
-        PORT = getenv('HBNB_API_PORT')
-    else:
-        PORT = '5000'
-    app.run(host=HOST, port=PORT, threaded=True)
+if __name__ == '__main__':
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
