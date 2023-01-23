2aa4a768da5c22f715f148ed28e57cb190d9607b
Akinola <akinolatofunmi04@gmail.com>
2023-01-23 11:29:16 +0000

    Fix abort statuscode

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -12,8 +12,7 @@ from flask import jsonify, abort, request, make_response
 from api.v1.views import app_views
 
 
-@app_views.route(
-        '/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
+@app_views.route('/cities/<city_id>/places', methods=['GET'])
 def get_places(city_id):
     """ Retrieves the list of all Place objects """
     city = models.storage.get(City, city_id)
@@ -49,7 +48,7 @@ def get_place(place_id):
 def create_place(city_id):
     """ Create a new instance of state """
     if not request.json:
-        abort(404, description='Not a JSON')
+        abort(400, description='Not a JSON')
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
