ae4d5f97cbf9fe349f84cde9c41b9a35db5d532e
Akinola <akinolatofunmi04@gmail.com>
2023-01-23 19:07:30 +0000

    Bring back the strict slashes fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,8 +11,9 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+# app.url_map.strict_slashes = False
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
